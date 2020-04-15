72309c43e3a20c545d4dce508b11b7e8b91ea496
Sivakumar <sivakumarks@gmail.com>
2020-04-15 14:57:49 +1000

    Tuples

--- turtle.py
+++ turtle.py
@@ -3,4 +3,58 @@ wn = turtle.Screen()
 wn.bgcolor("yellow")
 alex = turtle.Turtle()
 alex.forward(150)
-alex.left(6
+alex.left(60)
+alex.forward(75)
+wn.exitonclick()
+
+
+
+
+import turtle
+
+wn = turtle.Screen()
+wn.bgcolor("lightgreen")        # set the window background color
+
+tess = turtle.Turtle()
+tess.color("blue")              # make tess blue
+tess.pensize(3)                 # set the width of her pen
+
+tess.forward(50)
+tess.left(120)
+tess.forward(50)
+
+wn.exitonclick()                # wait for a user click on the canvas
+
+
+import turtle
+wn = turtle.Screen()
+alex = turtle.Turtle()
+alex.right(90)
+alex.forward(150)
+alex.left(90)
+alex.forward(75)
+wn.exitonclick()
+
+import turtle
+wn = turtle.Screen()
+alex = turtle.Turtle()
+alex.left(90);
+alex.forward(150)
+alex.left(90)
+alex.forward(75)
+wn.exitonclick()
+
+
+import turtle
+wn = turtle.Screen()
+wn.bgcolor("green")
+jamal = turtle.Turtle()
+jamal.color("white")
+jamal.pensize(10)
+jamal.left(90)
+jamal.forward(150)
+jamal.left(90)
+jamal.forward(50)
+jamal.right(180)
+jamal.forward(100)
+wn.exitonclick()
