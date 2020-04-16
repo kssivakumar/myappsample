32570657cf425a382da3375ea59943e8fe4c638d
Sivakumar <sivakumarks@gmail.com>
2020-04-16 14:37:49 +1000

    forloop learning

--- turtle.py
+++ turtle.py
@@ -58,3 +58,54 @@ jamal.forward(50)
 jamal.right(180)
 jamal.forward(100)
 wn.exitonclick()
+
+
+import turtle
+wn = turtle.Screen()             # Set up the window and its attributes
+wn.bgcolor("lightgreen")
+
+
+tess = turtle.Turtle()           # create tess and set his pen width
+tess.pensize(5)
+
+alex = turtle.Turtle()           # create alex
+alex.color("hotpink")
+alex.pensize(7)                   # set his color
+
+tess.forward(80)                 # Let tess draw an equilateral triangle
+tess.left(120)
+tess.forward(80)
+tess.left(120)
+tess.forward(80)
+tess.left(120)                   # complete the triangle
+
+tess.right(180)                  # turn tess around
+tess.forward(80)                 # move her away from the origin so we can see alex
+
+alex.forward(50)                 # make alex draw a square
+alex.left(90)
+alex.forward(50)
+alex.left(90)
+alex.forward(50)
+alex.left(90)
+alex.forward(50)
+alex.left(90)
+
+wn.exitonclick()
+
+
+import turtle
+wn = turtle.Screen()
+jamal = turtle.Turtle()
+jamal.pensize(10)
+jamal.color("blue")
+jamal.right(90)
+jamal.forward(150)
+jamal.left(90)
+jamal.forward(75)
+tina=turtle.Turtle()
+tina.pensize(10)
+tina.color("orange")
+tina.left(180)
+tina.forward(75)
+wn.exitonclick()
